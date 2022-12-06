e4d544d8e1a839092ff09364029e1cefc378afcc
haroldsr <advinculaharoldl.official@gmail.com>
2022-12-06 22:53:34 +0800

    add handleClick

--- imports/ui/Board.jsx
+++ imports/ui/Board.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 
 export const Board = () => {
+    const handleClick = (e) => {
+        if (e.target.textContent === "") {
+            e.target.textContent = "X"
+        } else {
+            e.target.textContent = "O"
+        }
+    }
+
     return (
         <div className='board'>
-            <div className='board--box' onClick={(e)=> console.log(e.target.textContent = "Nice")}>1</div>
-            <div className='board--box'>2</div>
-            <div className='board--box'>3</div>
-            <div className='board--box'>4</div>
-            <div className='board--box'>5</div>
-            <div className='board--box'>6</div>
-            <div className='board--box'>7</div>
-            <div className='board--box'>8</div>
-            <div className='board--box'>9</div>
+            <div className='board--box' onClick={handleClick}>1</div>
+            <div className='board--box' onClick={handleClick}>2</div>
+            <div className='board--box' onClick={handleClick}>3</div>
+            <div className='board--box' onClick={handleClick}>4</div>
+            <div className='board--box' onClick={handleClick}>5</div>
+            <div className='board--box' onClick={handleClick}>6</div>
+            <div className='board--box' onClick={handleClick}>7</div>
+            <div className='board--box' onClick={handleClick}>8</div>
+            <div className='board--box' onClick={handleClick}>9</div>
         </div>
     )
 }
--- imports/ui/Button.jsx
+++ imports/ui/Button.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 export const Button = () => {
     return (
-        <div className='button'>
-            <button>START</button>
-            <button>RESET</button>
+        <div className="player">
+            <div className='button'>
+                <div>PLAYER 1</div>
+                    <button>START</button>
+                    <button>RESET</button>
+                <div>PLAYER 2</div> 
+            </div>
         </div>
     )
 }
