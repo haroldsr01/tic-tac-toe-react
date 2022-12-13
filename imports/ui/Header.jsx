ac57d9edf36f329d656fb37983ef038b8c9770b6
haroldsr <advinculaharoldl.official@gmail.com>
2022-12-13 17:06:53 +0800

    update meteor

--- imports/ui/Header.jsx
+++ imports/ui/Header.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export const Header = () => {
     return (
         <header className="header">
-            <h1>BASICJS <br></br><small>by haroldsr</small></h1>
+            <h1>BASICJS <br></br><small>presents by haroldsr</small></h1>
             <h2>TIC-TAC-TOE GAME</h2>            
         </header>
     )
