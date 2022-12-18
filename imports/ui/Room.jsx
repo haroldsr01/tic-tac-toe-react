4e52aa9c5ee83f125f2fc4783bd036d9be7e4547
haroldsr <advinculaharoldl.official@gmail.com>
2022-12-18 16:45:52 +0800

    add btn conditional prop to btn-play

--- imports/ui/Room.jsx
+++ imports/ui/Room.jsx
@@ -20,7 +20,7 @@ export const Room = (props) => {
             {isClick ? <td><button disabled={props.room.players[1] ? true : false} className='btn-joinroom' onClick={()=>{props.joinRoom(props.room._id,props.user.username);toggleClick()}}>JOIN</button></td>:
             <td><button disabled={props.user.username !== props.room.players[1] ? true : false } className='btn-leave' onClick={()=>{props.leaveRoom(props.room._id);toggleClick()}}>LEAVE</button></td>
             }                    
-            <td><button className='btn-play'>PLAY</button></td>
+            <td><button disabled={props.user.username !==props.room.host ? true : false} className='btn-play'>PLAY</button></td>
             <td><button disabled={props.user.username !== props.room.host ? true : false } className='btn-close' onClick={()=>props.closeRoom(props.room._id,props.room.host)}>CLOSE</button></td>            
         </tr>
 )
